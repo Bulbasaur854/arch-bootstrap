38953448ce35e56f106913818a24c1690af148e0
Bul8a54urArch <tairshechter1118@gmail.com>
2025-11-20 12:38:17 +0200

    executable and pywal reorder

--- scripts/03-pywal.sh
+++ scripts/03-pywal.sh
@@ -8,14 +8,15 @@ if ! pacman -Qi "python-pywal" &> /dev/null 2>&1; then
     echo "Could not detect pywal, installing now..."
     sudo pacman -S --noconfirm --needed python-pywal
 fi
+mkdir -p "$HOME/.cache/wal"
+echo "Pywal setup is complete."
 
 if ! pacman -Qi "hyprpaper" &> /dev/null 2>&1; then
     echo "Could not detect hyprpaper, installing now..."
     sudo pacman -S --noconfirm --needed hyprpaper
 fi
-mkdir -p "$HOME/.cache/wal"
-echo "Pywal setup is complete."
 
-# Pick wallpaper and generate theme
+# Generate theme colors,
+# the actual wallpaper setup is done by `hyprpaper.cong`
 wall="$HOME/.config/backgrounds/group_of_pink_flowers.jpg"
 wal -q -i "$wall"
