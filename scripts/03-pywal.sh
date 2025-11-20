854e93b7016c7c3094405ec34197d7345c215a24
Bul8a54urArch <tairshechter1118@gmail.com>
2025-11-20 17:46:01 +0200

    strip hyprpaper setup

--- scripts/03-pywal.sh
+++ scripts/03-pywal.sh
@@ -4,19 +4,15 @@
 # ==========================================================
 set -e
 
-if ! pacman -Qi "python-pywal" &> /dev/null 2>&1; then
-    echo "Could not detect pywal, installing now..."
-    sudo pacman -S --noconfirm --needed python-pywal
+wall="$HOME/.config/backgrounds/group_of_pink_flowers.jpg"
+
+if ! pacman -Qi "python-pywal" &>/dev/null 2>&1; then
+  echo "Could not detect pywal, installing now..."
+  sudo pacman -S --noconfirm --needed python-pywal
 fi
 mkdir -p "$HOME/.cache/wal"
-echo "Pywal setup is complete."
-
-if ! pacman -Qi "hyprpaper" &> /dev/null 2>&1; then
-    echo "Could not detect hyprpaper, installing now..."
-    sudo pacman -S --noconfirm --needed hyprpaper
-fi
-
 # Generate theme colors,
 # the actual wallpaper setup is done by `hyprpaper.cong`
-wall="$HOME/.config/backgrounds/group_of_pink_flowers.jpg"
 wal -q -i "$wall"
+
+echo "Pywal setup is complete."
